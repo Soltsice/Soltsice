f35a75b2204131bd98ae8d51dca62e9e9352a806
Victor Baybekov <vbaybekov@gmail.com>
2017-09-11 18:59:15 +0300

    Add DBrainToken and DBrainCrowdsale basic contracts

--- contracts/DBrainToken.sol
+++ contracts/DBrainToken.sol
@@ -10,7 +10,38 @@ import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
 // import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 // import 'zeppelin-solidity/contracts/token/ERC20Basic.sol';
 
+// TODO this is WIP and not reviewed, need review all parent chain details, e.g. if need to override something else
+
 contract DBrainToken is MintableToken, Pausable {
+
+  // Constants
+  // =========
+  string public constant name = "DBrain Token";
+  string public constant symbol = "DBN";
+  uint256 public constant decimals = 18;
+
+  uint256 constant INITIAL_SUPPLY = 1400000 * 1e18; // 1.4M tokens, 18 decimals
+
+
+  // State variables
+  // ===============
+
+  address public multisig;
+
+  // 
+  bool public paused = true;
+
+  // Events
+  // ==================
+
+  // Constructor
+  // ============
+
+  function DBrainToken(address _multisig) {
+    multisig = _multisig;
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY; // TODO temp, set pool to owner, later will move this logic to crowdsale
+  }
 
   function transfer(address _to, uint256 _value) whenNotPaused returns (bool) {
     return super.transfer(_to, _value);
@@ -19,4 +50,6 @@ contract DBrainToken is MintableToken, Pausable {
   function transferFrom(address _from, address _to, uint256 _value) whenNotPaused returns (bool) {
     return super.transferFrom(_from, _to, _value);
   }
+
+  
 }
